597e13e6cc6837fbd2502d25393b35e9629e1d27
汪 <wang@wangdeMacBook-Pro.local>
2017-04-07 15:51:46 +0800

    new

--- src/graph.py
+++ src/graph.py
@@ -37,40 +37,46 @@ filter_shape = [filter_width, word_dim + overlap_ndim, 1, kernals]
 numpy_rng = numpy.random.RandomState(123)
 vocab_emb_overlap = tf.Variable(numpy_rng.randn(overlap_word_max_id+1, overlap_ndim)*0.25, name='W')
 t_vocab_emb_overlap = tf.random_normal(shape=[overlap_word_max_id+1, overlap_ndim])
-x_q = tf.placeholder(tf.int32, [None, q_max_sent_size], name='x_q')
-x_q_overlap = tf.placeholder(tf.int32, [None, q_max_sent_size], name='x_q_overlap')
-x_a = tf.placeholder(tf.int32, [None, a_max_sent_size], name='x_a')
-x_a_overlap = tf.placeholder(tf.int32, [None, a_max_sent_size], name='x_a_overlap')
+with tf.name_scope('input'):
+	x_q = tf.placeholder(tf.int32, [None, q_max_sent_size], name='x_q')
+	x_q_overlap = tf.placeholder(tf.int32, [None, q_max_sent_size], name='x_q_overlap')
+	x_a = tf.placeholder(tf.int32, [None, a_max_sent_size], name='x_a')
+	x_a_overlap = tf.placeholder(tf.int32, [None, a_max_sent_size], name='x_a_overlap')
 
 #Define Net
-lookup_words = layer.LookupTable(W=vocab_emb, batch_size=batch_size, filter_width=filter_width, width=word_dim)
-lookup_words_overlap = layer.LookupTable(W=vocab_emb_overlap, batch_size=batch_size, filter_width=filter_width, width=overlap_ndim)
-lookup_words = layer.ParallelLookupTable([lookup_words, lookup_words_overlap])
-conv = layer.Conv2d(filter_shape=filter_shape, filter_width=filter_width, batch_size=batch_size, height=q_max_sent_size+2*(filter_width-1), width=overlap_ndim+word_dim)
-non_line = layer.Activation(activation=tf.tanh, nkernals=kernals)
-max_pool = layer.Maxpool(ksize=[1, q_max_sent_size+filter_width-1, 1, 1])
-flatten = layer.Flatten(batch_size=batch_size)
-net_q = layer.FeedForwardNet([lookup_words, conv, non_line, max_pool, flatten])
-net_q.set_input([x_q, x_q_overlap])
+with tf.name_scope('net_q'):
+	lookup_words = layer.LookupTable(W=vocab_emb, batch_size=batch_size, filter_width=filter_width, width=word_dim)
+	lookup_words_overlap = layer.LookupTable(W=vocab_emb_overlap, batch_size=batch_size, filter_width=filter_width, width=overlap_ndim)
+	lookup_words = layer.ParallelLookupTable([lookup_words, lookup_words_overlap])
+	conv = layer.Conv2d(filter_shape=filter_shape, filter_width=filter_width, batch_size=batch_size, height=q_max_sent_size+2*(filter_width-1), width=overlap_ndim+word_dim)
+	non_line = layer.Activation(activation=tf.tanh, nkernals=kernals)
+	max_pool = layer.Maxpool(ksize=[1, q_max_sent_size+filter_width-1, 1, 1])
+	flatten = layer.Flatten(batch_size=batch_size)
+	net_q = layer.FeedForwardNet([lookup_words, conv, non_line, max_pool, flatten])
+	net_q.set_input([x_q, x_q_overlap])
 
-lookup_words = layer.LookupTable(W=vocab_emb, batch_size=batch_size, filter_width=filter_width, width=word_dim)
-lookup_words_overlap = layer.LookupTable(W=vocab_emb_overlap, batch_size=batch_size, filter_width=filter_width, width=overlap_ndim)
-lookup_words = layer.ParallelLookupTable([lookup_words, lookup_words_overlap])
-conv = layer.Conv2d(filter_shape=filter_shape, filter_width=filter_width, batch_size=batch_size, height=a_max_sent_size+2*(filter_width-1), width=overlap_ndim+word_dim)
-non_line = layer.Activation(tf.tanh, nkernals=kernals)
-max_pool = layer.Maxpool(ksize=[1, a_max_sent_size+filter_width-1, 1, 1])
-flatten = layer.Flatten(batch_size=batch_size)
-net_a = layer.FeedForwardNet([lookup_words, conv, non_line, max_pool, flatten])
-net_a.set_input([x_a, x_a_overlap])
+with tf.name_scope('net_a'):
+	lookup_words = layer.LookupTable(W=vocab_emb, batch_size=batch_size, filter_width=filter_width, width=word_dim)
+	lookup_words_overlap = layer.LookupTable(W=vocab_emb_overlap, batch_size=batch_size, filter_width=filter_width, width=overlap_ndim)
+	lookup_words = layer.ParallelLookupTable([lookup_words, lookup_words_overlap])
+	conv = layer.Conv2d(filter_shape=filter_shape, filter_width=filter_width, batch_size=batch_size, height=a_max_sent_size+2*(filter_width-1), width=overlap_ndim+word_dim)
+	non_line = layer.Activation(tf.tanh, nkernals=kernals)
+	max_pool = layer.Maxpool(ksize=[1, a_max_sent_size+filter_width-1, 1, 1])
+	flatten = layer.Flatten(batch_size=batch_size)
+	net_a = layer.FeedForwardNet([lookup_words, conv, non_line, max_pool, flatten])
+	net_a.set_input([x_a, x_a_overlap])
 
-pair_combine = layer.PairCombine(shape1=kernals, shape2=kernals)
-pair_combine.set_input([net_q.output, net_a.output])
+with tf.name_scope('pair_combine'):
+	pair_combine = layer.PairCombine(shape1=kernals, shape2=kernals)
+	pair_combine.set_input([net_q.output, net_a.output])
 
-hidden_layer = layer.Linear(n_in=2*kernals+1, n_out=2*kernals+1)
-hidden_layer.set_input(pair_combine.output)
+with tf.name_scope('hiddent_layer'):
+	hidden_layer = layer.Linear(n_in=2*kernals+1, n_out=2*kernals+1)
+	hidden_layer.set_input(pair_combine.output)
 
-lr_layer = layer.LR(n_in=2*kernals+1, n_out = n_class)
-lr_layer.set_input(hidden_layer.output)
+with tf.name_scope('LR'):
+	lr_layer = layer.LR(n_in=2*kernals+1, n_out = n_class)
+	lr_layer.set_input(hidden_layer.output)
 
 #Save Graph
 tf.add_to_collection('x_q', x_q)
@@ -85,3 +91,6 @@ with tf.Session() as sess:
 	sess.run(init)
 	saver = tf.train.Saver()
 	saver.export_meta_graph('qa.graph')
+	graph_writer = tf.summary.FileWriter('./tensorboard', sess.graph)
+
+graph_writer.close()
